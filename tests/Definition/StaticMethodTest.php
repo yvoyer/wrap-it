8923c9d690f32fc39a172e0931fbec3b1e18a04f
Yannick Voyer <yan.voyer@gmail.com>
2018-11-25 10:30:58 -0500

    Backup

---
+++ tests/Definition/StaticMethodTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Star\WrapIt\Definition;
+
+use PHPUnit\Framework\TestCase;
+
+final class StaticMethodTest extends TestCase
+{
+    /**
+     * @var StaticMethod
+     */
+    private $_staticMethod;
+
+    public function setUp()
+    {
+        $this->markTestIncomplete('todo');
+    }
+
+    public function test_it_should_do_something()
+    {
+        $this->fail('test something');
+    }
+}
